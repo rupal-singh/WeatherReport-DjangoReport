940c4db9e777c4ee37cdeac83b0245787abf52ab
Rupal Singh <rupalsingh617@gmail.com>
2021-12-12 12:53:13 +0530

    added requiremnet.txt and comments in view.py

--- WeatherReport/weatherapp/views.py
+++ WeatherReport/weatherapp/views.py
@@ -1,40 +1,45 @@
-from django.shortcuts import redirect, render
-from geopy.geocoders import Nominatim
-import requests
-from weatherapp.models import WeatherReport
 from datetime import datetime
+
+import requests
+from django.shortcuts import render
+from geopy.geocoders import Nominatim
+
+from weatherapp.models import WeatherReport
 
 # Create your views here.
 url = "https://api.weather.gov/points/"
 geolocator = Nominatim(user_agent="geoapiExercises")
 
+
+# render index.html webpage
 def index(request):
-    if request.method == "POST":
+    if request.method == "POST":     # if post
         try:
-            lati = request.POST.get('lati')
-            longi = request.POST.get('longi')
-            weather_data = requests.get(f'{url}{lati},{longi}').json()
-            forecast = requests.get(weather_data['properties']["forecast"]).json()
-            location = geolocator.reverse(lati+","+longi)
-            temp = forecast["properties"]['periods'][0]['temperature']
-            unit = forecast["properties"]['periods'][0]['temperatureUnit']
-            windSpeed = forecast["properties"]['periods'][0]['windSpeed']
-            direction = forecast["properties"]['periods'][0]['windDirection']
-            desc = forecast["properties"]['periods'][0]['detailedForecast']
-            con = WeatherReport(city_name=location, latitude=lati, longitude=longi, temperature=temp, date_of_entry=datetime.today())
+            lati = request.POST.get('lati')         # get latitude
+            longi = request.POST.get('longi')       # get longitude
+            weather_data = requests.get(f'{url}{lati},{longi}').json()  # get json data of specific location
+            forecast = requests.get(weather_data['properties']["forecast"]).json()  # get weather forecast data of the location
+            location = geolocator.reverse(lati + "," + longi)   # get name of the location using latitude and longitude
+            temp = forecast["properties"]['periods'][0]['temperature']  # get temperature
+            unit = forecast["properties"]['periods'][0]['temperatureUnit']  # get unit of temperature
+            windSpeed = forecast["properties"]['periods'][0]['windSpeed']   # get wind speed
+            direction = forecast["properties"]['periods'][0]['windDirection']   # get direction of wind
+            desc = forecast["properties"]['periods'][0]['detailedForecast'] # get small summary
+            con = WeatherReport(city_name=location, latitude=lati, longitude=longi, temperature=temp,
+                                date_of_entry=datetime.today()) # commit to database
             con.save()
             context = {
-                        'city': location,
-                        'temp': f"{temp} {unit}",
-                        'windSpeed' : windSpeed,
-                        'direction' : direction,
-                        'desc' : desc
-                    }
-            return render(request, "index.html", context)
-        except Exception as e:
+                'city': location,
+                'temp': f"{temp} {unit}",
+                'windSpeed': windSpeed,
+                'direction': direction,
+                'desc': desc
+            }
+            return render(request, "index.html", context)   # render index.html with weather info
+        except Exception as e:      # if data not found then except the Exception
             context = {
-                            'Error' : "Data not found"
-                        }
+                'Error': "Data not found"
+            }
             return render(request, "index.html", context)
-    
-    return render(request, 'index.html')
+
+    return render(request, 'index.html')    # render index.html page
